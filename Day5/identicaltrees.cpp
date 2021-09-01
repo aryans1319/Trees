747bd170227020537d2baa827dbcacd5d8ffd01d
Aryan Shaw <aryan4p130202@gmail.com>
2021-09-02 02:14:32 +0530

    Create identicaltrees.cpp

---
+++ Day5/identicaltrees.cpp
@@ -0,0 +1,9 @@
+  
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p) return !q;
+        if(!q) return !p;
+        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
